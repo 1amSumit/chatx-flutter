64fa053db4f890a794d15ae5bad267d7aea227cd
1amSumit <sj779619@gmail.com>
2023-10-05 18:31:43 +0530

    welcome screen rehsiter screenn implemented

---
+++ lib/screens/registerScreen.dart
@@ -0,0 +1,100 @@
+import 'package:chatx/screens/login.dart';
+import 'package:chatx/screens/signup.dart';
+import 'package:flutter/material.dart';
+
+class LoginRegisterScreen extends StatefulWidget {
+  const LoginRegisterScreen({super.key});
+
+  @override
+  State<LoginRegisterScreen> createState() => _LoginRegisterScreenState();
+}
+
+class _LoginRegisterScreenState extends State<LoginRegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(10.0),
+              ),
+              SizedBox(
+                width: 140.0,
+                height: 140.0,
+                child: Image.asset(
+                  'images/logo-chatx.png',
+                  fit: BoxFit.contain,
+                ),
+              ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Text(
+                "Chat X_",
+                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: Material(
+              color: Colors.orange[300],
+              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              elevation: 5.0,
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return LogIn();
+                      },
+                    ),
+                  );
+                },
+                minWidth: 300.0,
+                height: 42.0,
+                child: Text(
+                  'Log In',
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: Material(
+              color: Colors.orange,
+              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              elevation: 5.0,
+              child: MaterialButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return SignUp();
+                      },
+                    ),
+                  );
+                },
+                minWidth: 300.0,
+                height: 42.0,
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/welcome.dart
@@ -0,0 +1,57 @@
+import 'package:chatx/screens/registerScreen.dart';
+import 'package:flutter/material.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image(
+              image: AssetImage('images/logo-chatx.png'),
+            ),
+          ),
+          SizedBox(
+            height: 60.0,
+          ),
+          Container(
+            child: SizedBox(
+              width: 250.0,
+              height: 40.0,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return LoginRegisterScreen();
+                      },
+                    ),
+                  );
+                },
+                child: Text(
+                  "Agree and continue",
+                  style: TextStyle(color: Colors.black, fontSize: 16.0),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll<Color>(
+                    Color(0xFFFEF6E9),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
