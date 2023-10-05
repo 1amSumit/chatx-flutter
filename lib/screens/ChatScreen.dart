56f2908887bbbd7f232e79f43c6bc6bf9e53467d
1amSumit <sj779619@gmail.com>
2023-10-05 21:22:57 +0530

    chat screen done

--- lib/screens/ChatScreen.dart
+++ lib/screens/ChatScreen.dart
@@ -13,48 +13,102 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFC6AD13),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: Container(
-              color: Color(0xFFC6AD13),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                  ),
-                  Icon(FontAwesomeIcons.arrowLeft),
-                  SizedBox(
-                    width: 120.0,
-                  ),
-                  Text(
-                    "Chat",
-                    style: TextStyle(
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'Source Sans 3',
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Container(
+                color: Color(0xFFC6AD13),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
                     ),
-                  ),
-                ],
+                    Icon(FontAwesomeIcons.arrowLeft),
+                    SizedBox(
+                      width: 120.0,
+                    ),
+                    Text(
+                      "Chat",
+                      style: TextStyle(
+                        fontSize: 32.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Source Sans 3',
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 4,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xFFC6BFA2),
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50.0),
-                    topRight: Radius.circular(50.0)),
+            Expanded(
+              flex: 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFFC6BFA2),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0)),
+                ),
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 7,
+                      child: Container(
+                        child: Column(),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                        child: TextField(
+                          onChanged: (value) {},
+                          decoration: InputDecoration(
+                            suffixIcon: Padding(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 10.0, horizontal: 20.0),
+                              child: Icon(
+                                FontAwesomeIcons.paperPlane,
+                                color: Colors.yellow[600],
+                              ),
+                            ),
+                            filled: true,
+                            fillColor: Color(0xFFB2AE94),
+                            hintText: "Enter your Email",
+                            hintStyle: TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Source Sans 3',
+                              fontWeight: FontWeight.bold,
+                            ),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(32.0),
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color(0xFFB2AE94), width: 1.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color(0xFFB2AE94), width: 2.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
-              child: Column(
-                children: [],
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
