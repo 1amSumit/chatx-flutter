4fc3b1c03796bc5171f90165c6c34219988da9a2
1amSumit <sj779619@gmail.com>
2023-10-05 18:46:49 +0530

    Sign up and login page implemeted

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -10,6 +10,95 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 40.0),
+              child: SizedBox(
+                child: Image.asset('images/logo-chatx.png'),
+                width: 200.0,
+                height: 200.0,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40.0,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: TextField(
+              onChanged: (value) {},
+              decoration: InputDecoration(
+                hintText: "Enter your Email",
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.yellow, width: 1.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.orange, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 20.0, right: 20.0),
+            child: TextField(
+              onChanged: (value) {},
+              decoration: InputDecoration(
+                hintText: "Enter your Passowrd",
+                contentPadding:
+                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(32.0),
+                  ),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.yellow, width: 1.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.orange, width: 2.0),
+                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 18.0,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0),
+            child: Material(
+              color: Colors.orange[300],
+              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              elevation: 5.0,
+              child: MaterialButton(
+                onPressed: () {},
+                minWidth: 300.0,
+                height: 42.0,
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
